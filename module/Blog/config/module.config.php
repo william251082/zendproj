7dd8f3186f65bf94c74f9c613f0089d13d560cde
williamdelrosario <williamdelrosario@yahoo.com>
2017-12-03 19:38:29 +0100

    Added Blog module.config and IndexController files

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -1,7 +1,52 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: williamdelrosario
- * Date: 03/12/2017
- * Time: 19:22
- */
+
+namespace Blog;
+
+return array(
+    'router' => array(
+        'routes' => array(
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /blog/:controller/:action
+            'blog' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/blog',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Blog\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'controllers' => array(
+        'invokables' => array(
+            'Blog\Controller\Index' => Controller\IndexController::class
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
